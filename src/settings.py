15568431496de1e531eaaa7ae19a69c7864f0f14
Ray Hil <rayhil@MacBook-Pro-Ray.local>
2024-02-08 19:49:14 +0800

    upd

---
+++ src/settings.py
@@ -0,0 +1,110 @@
+class settings:
+    # Инициализируем пустыми
+    __first_name = None
+    __inn = None
+    __bik = None
+    __account = None
+    __cor_account = None
+    __name = None
+    __ownship_type = None
+
+    @property
+    def first_name(self):
+        return self.__first_name
+    
+    @first_name.setter
+    def first_name(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("Некорректный аргумент!")
+        
+        self.__first_name = value.strip()
+        
+    # ИНН
+    @property
+    def inn(self):
+        return self.__inn
+    
+    @inn.setter
+    def inn(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("ERROR: Некорректный аргумент!")
+        
+        if len(value) != 12:
+            raise Exception("ERROR: Длина ИНН не равна 12!")
+        
+        self.__inn = value.strip()
+
+    # Кор. счёт
+    @property
+    def cor_account(self):
+        return self.__cor_account
+    
+    @cor_account.setter
+    def cor_account(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("Некорректный аргумент!")
+        
+        if len(value) != 11:
+            raise Exception("ERROR: Длина кор. счёта не равна 11!")
+        
+        self.__cor_account = value.strip()
+    
+    # БИК
+    @property
+    def bik(self):
+        return self.__bik
+    
+    @bik.setter
+    def bik(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("Некорректный аргумент!")
+        
+        if len(value) != 9:
+            raise Exception("ERROR: Длина БИК не равна 9!")
+        
+        self.__bik = value.strip()
+
+    # Счёт
+    @property
+    def account(self):
+        return self.__account
+    
+    @account.setter
+    def account(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("Некорректный аргумент!")
+
+        if len(value) != 11:
+            raise Exception("ERROR: Длина счёта не равна 11!")
+        
+        self.__account = value.strip()
+
+    # Наименование
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("Некорректный аргумент!")
+        
+        if str == "":
+            raise Exception("ERROR: Пустое наименование!")
+        
+        self.__name = value.strip()
+
+    # Вид собственности
+    @property
+    def ownship_type(self):
+        return self.__ownship_type
+    
+    @ownship_type.setter
+    def ownship_type(self, value: str):
+        if not isinstance(value, str):
+            raise Exception("Некорректный аргумент!")
+        
+        if len(value) != 5:
+            raise Exception("ERROR: Длина вида собственности не равна 5!")
+        
+        self.__ownship_type = value.strip()
---
+++ src/settings_manager.py
@@ -0,0 +1,72 @@
+import os
+import uuid
+import json
+from src.settings import settings
+
+class settings_manager(object):
+    __file_name = "settings.json"
+    # Уникальный номер
+    __unique_number = None
+    
+    __data = {}
+    # Настройки инстанс
+    __settings = settings()
+
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(settings_manager, cls).__new__(cls)
+    
+        return cls.instance
+    
+    def convert(self):
+        if len(self.__data) == 0:
+            raise Exception("ERROR: Невозможно создать объект типа settings.py")
+        
+        fields = dir(self.__settings.__class__)
+
+        for field in fields:
+            # Проверяем есть ли такое значение у объекта
+            if not field in self.__data.keys():
+                continue
+
+            setattr(self.__settings, field, self.__data[field])
+
+        return self.__settings
+        
+
+    def __init__(self) -> None:
+        self.__unique_number = uuid.uuid4()
+
+    def open(self, file_name: str) -> bool:
+        if not isinstance(file_name, str):
+            raise Exception("ERROR: Неверный аргумент file_name")
+
+        if file_name == "":
+            raise Exception("ERROR: Неверный аргумент file_name")
+        
+        self.__file_name = file_name.strip()
+
+        try:
+            self.__open()
+            return True
+        except:
+            return False
+
+
+    def __open(self):
+        file_path = os.path.split(__file__)
+        settings_file = "%s/%s" % (file_path[0], self.__file_name)
+
+        if not os.path.exists(settings_file):
+            raise Exception("ERROR: невозможно загрузить настройки")
+        
+        with open(settings_file, "r") as read_file:
+            self.__data = json.load(read_file)
+
+    @property
+    def number(self):
+        return str(self.__unique_number.hex)
+    
+    @number.setter
+    def number(self, value: int) -> str:
+        self.__unique_number = value
