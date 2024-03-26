1aaeb4b52a182aad4beea53cc113eea8466fb197
Ray Hil <rayhil@MacBook-Pro-Ray.local>
2024-03-26 09:50:54 +0800

    storage

--- src/Logics/Process/abstract_process.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-class abstract_process(ABC):
-    operations = {True: 1, False: -1}
-
-    @abstractmethod
-    def create(self):
-        pass
--- src/Logics/Process/process_storage_turn.py
+++
@@ -1,7 +0,0 @@
-from Src.Logics.Process.abstract_process import abstract_process
-from Src.Models.storage_turn_model import storage_turn_model
-
-class process_storage_turn(abstract_process):
-    @classmethod
-    def create(cls, journal: list[storage_turn_model]):
-        return []
