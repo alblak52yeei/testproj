a28fd0a0dc9caa14ae0bae73ea5bdd7339068af9
Ray Hil <rayhil@MacBook-Pro-Ray.local>
2024-05-19 22:01:47 +0800

    fix

--- src/errors.py
+++ src/errors.py
@@ -144,6 +144,14 @@ class error_proxy:
             item.error = message
             item.log_type = log_type
 
+            observer_item.item = item           
+
+        observer_item = storage_observer.get( storage_observer .console_log_service_key() )
+        if observer_item is not None:
+            item = error_proxy()
+            item.error = message
+            item.log_type = log_type
+
             observer_item.item = item
-            storage_observer.raise_event( "write_log" )
-                
+            
+        storage_observer.raise_event( "write_log" )
